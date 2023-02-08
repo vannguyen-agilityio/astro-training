8b344ae98485155d2ea92b963e02b3991e3f3f6e
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-09 03:04:39 +0700

    feat: implement component Button

--- travel-landing/tailwind.config.cjs
+++ travel-landing/tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    'bg-primary',
+    'bg-rarely',
+    'min-w-button'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#ffffff',
+        lightPrimary: '#fafafa',
+        lightSecondary: '#f2f2f2',
+        dark: '#34343f',
+        primary: '#f89f4e',
+        rarely: '#02a29f'
+      },
+      minWidth: {
+        'button': '98px',
+      },
+    },
   },
   plugins: [],
 }
