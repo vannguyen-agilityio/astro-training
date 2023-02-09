dfad57f40ee2dc2fe9b94126c050c42bb3d8a3a4
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-09 19:47:10 +0700

    feat: update font family, font size for text button component

--- travel-landing/tailwind.config.cjs
+++ travel-landing/tailwind.config.cjs
@@ -4,7 +4,14 @@ module.exports = {
   safelist: [
     'bg-primary',
     'bg-rarely',
-    'min-w-button'
+    'min-w-btn',
+    'min-w-btn-primary',
+    'min-w-btn-rarely',
+    'min-w-btn-secondary',
+    'min-h-btn',
+    'min-h-btn-primary',
+    'min-h-btn-secondary',
+    'min-h-btn-rarely',
   ],
   theme: {
     extend: {
@@ -17,8 +24,36 @@ module.exports = {
         rarely: '#02a29f'
       },
       minWidth: {
-        'button': '98px',
+        'btn': '98px',
+        'btn-primary': '310px',
+        'btn-rarely': '110px',
+        'btn-secondary': '228px'
       },
+      minHeight: {
+        'btn': '46px',
+        'btn-primary': '110px',
+        'btn-rarely': '38px',
+        'btn-secondary': '56px'
+      },
+      fontFamily: {
+        primary: ['Abel', 'sans-serif']
+      },
+      fontSize: {
+        'size-xs': '15px',
+        'size-sm': '19px',
+        'size-lg': '20px',
+        'size-xl': '22px',
+        'size-2xl': '25px',
+        'size-3xl': '48px'
+      },
+      lineHeight: {
+        'xs': '19px',
+        'sm': '24px',
+        'lg': '25px',
+        'xl': '28px',
+        '2xl': '32px',
+        '3xl': '61px'
+      }
     },
   },
   plugins: [],
