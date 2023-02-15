ed7ac77e6ac83e69aeba67b419215393ed3822ed
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-15 15:31:03 +0700

    feat: implement destinations page

--- travel-landing/src/constants/common.ts
+++ travel-landing/src/constants/common.ts
@@ -14,3 +14,32 @@ export const IMAGES_HOME = {
   bgVideoMobile2x: '../images/home/webp/bg-video-mobile-2x.webp',
   bgVideoDefault: '../images/home/png/bg-video.png'
 };
+
+export const IMAGES_DESTINATIONS = {
+  bgDestiniesDesktop: '../images/destinations/webp/destinies.webp',
+  bgDestiniesMobile: '../images/destinations/webp/destinies-mobile.webp',
+  bgDestiniesMobile2x: '../images/destinations/webp/destinies.webp',
+  bgDestiniesDefault: '../images/destinations/png/destinies.png',
+
+  bgDestiniesOneDesktop: '../images/destinations/webp/destinies-one.webp',
+  bgDestiniesOneMobile: '../images/destinations/webp/destinies-one-mobile.webp',
+  bgDestiniesOneMobile2x: '../images/destinations/webp/destinies-one.webp',
+  bgDestiniesOneDefault: '../images/destinations/png/destinies-one.png',
+
+  bgDestiniesTwoDesktop: '../images/destinations/webp/destinies-two.webp',
+  bgDestiniesTwoMobile: '../images/destinations/webp/destinies-two-mobile.webp',
+  bgDestiniesTwoMobile2x: '../images/destinations/webp/destinies-two.webp',
+  bgDestiniesTwoDefault: '../images/destinations/png/destinies-two.png',
+
+  bgDestiniesThreeDesktop: '../images/destinations/webp/destinies-three.webp',
+  bgDestiniesThreeMobile:
+    '../images/destinations/webp/destinies-three-mobile.webp',
+  bgDestiniesThreeMobile2x: '../images/destinations/webp/destinies-three.webp',
+  bgDestiniesThreeDefault: '../images/destinations/png/destinies-three.png',
+
+  bgDestiniesFourDesktop: '../images/destinations/webp/destinies-four.webp',
+  bgDestiniesFourMobile:
+    '../images/destinations/webp/destinies-four-mobile.webp',
+  bgDestiniesFourMobile2x: '../images/destinations/webp/destinies-four.webp',
+  bgDestiniesFourDefault: '../images/destinations/png/destinies-four.png'
+};
