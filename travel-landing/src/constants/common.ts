8eb3c78909c0b20c2a11483d3015b88faf2791b1
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-17 02:35:03 +0700

    feat: update style cards pricing

--- travel-landing/src/constants/common.ts
+++ travel-landing/src/constants/common.ts
@@ -69,7 +69,7 @@ export const IMAGES_ABOUT = {
 
 export const PRICING_LIST = [
   {
-    className: 'min-h-card-one min-w-form-contact',
+    className: 'min-h-card-one',
     nameHotels: ['Hotel King Lux - Europe', 'Hotel Barchelor - France'],
     salary: 1000,
     imageUrls: {
@@ -80,7 +80,7 @@ export const PRICING_LIST = [
     }
   },
   {
-    className: 'sm:mx-4 min-h-card-two min-w-form-contact',
+    className: 'sm:mx-4 min-h-card-two',
     nameHotels: [
       'Hotel King Lux - Europe',
       'Hotel Barchelor - France',
@@ -96,7 +96,7 @@ export const PRICING_LIST = [
     }
   },
   {
-    className: 'min-h-card-three min-w-form-contact',
+    className: 'min-h-card-three',
     nameHotels: ['12 Destinies Random across Europe'],
     salary: 1300,
     imageUrls: {
